db74aed41c31fc069fcf3307383362a7acb1e29f
venkatgowrish07-hub <venkatgowrish07@gmail.com>
2026-05-11 14:52:41 +0530

    Add files via upload

---
+++ Design Sources/top.v
@@ -0,0 +1,15 @@
+module top(an,seg,clkout,clk,reset);
+
+output [3:0]an;
+output [6:0]seg;
+input clk,reset;
+
+output clkout;
+wire [31:0]result;
+wire [3:0]thousands,hundreds,tens,ones;
+
+clkDivider cd(clkout,clk,reset);
+processor risc(result,regwrite,clkout,reset);
+bintobcd con(thousands,hundreds,tens,ones,result);
+displaymux dm(an,seg,thousands,hundreds,tens,ones,clk);
+endmodule
